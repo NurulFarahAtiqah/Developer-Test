82712894a8d48c5f4bc1df079dbe7d833d4a75c4
Farahatiqah34 <37291111+Farahatiqah34@users.noreply.github.com>
2021-10-26 22:02:48 +0800

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -20,7 +20,7 @@ if ($conn->connect_error) {
 $user = "INSERT INTO users (id,name,email,token,created)VALUES('00003','Nurul Farah Atiqah','nurulfarahatiqah34@gmail.com','b4a3a48795595af525772777b4b3b86e','26-10-2021 15:55')";
 
 //table locations - id, user_id (int), district (varchar 255), state (varchar 255),country (varchar 255)
-$location = "INSERT INTO location(location_id,id,district,tate,country)VALUES(00001,00003,'Pekan','Pahang','Malaysia')";
+$location = "INSERT INTO location(location_id,id,district,state,country)VALUES(00001,00003,'Pekan','Pahang','Malaysia')";
 
 
 
